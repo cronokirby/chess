6225aafba806d4fbf34c4fd268018fb202c67ed7
Lúcás Meier <cronokirby@gmail.com>
2020-06-11 21:16:44 +0200

    Create logical chessboard class
    This represents the logic behind moving chess pieces, and not the
    visuals behind hovering, and what not.

--- src/chess.ts
+++ src/chess.ts
@@ -32,6 +32,17 @@ export class Board<T> {
   }
 
   /**
+   * Remove the value at a certain position.
+   *
+   * This does nothing if no value is present at that position.
+   *
+   * @param param0 the position to remove the value of
+   */
+  remove({ x, y }: Pos) {
+    this.repr[y][x] = null;
+  }
+
+  /**
    * Modify a certain position on the board
    *
    * @param pos the position to set
@@ -49,5 +60,68 @@ export class Board<T> {
    */
   get({ x, y }: Pos): T | null {
     return this.repr[y][x];
+  }
+}
+
+/**
+ * Represents the color of a chess piece
+ */
+enum Color {
+  Black,
+  White,
+}
+
+/**
+ * Represents what kind of piece we have
+ */
+enum PieceKind {
+  Knight,
+}
+
+/**
+ * Represents a complete piece, with color and kind
+ */
+interface Piece {
+  color: Color;
+  kind: PieceKind;
+}
+
+/**
+ * Represents the logical actions that can take place on a chess board.
+ *
+ * This doesn't contain all of the logic we need for displaying the board, as
+ * it's missing information like hovers, and what not, but that's handled at a separate
+ * layer.
+ */
+class LogicalChessBoard {
+  private readonly board: Board<Piece>;
+
+  /**
+   * Setup a new chess board
+   */
+  constructor() {
+    this.board = new Board(8);
+    this.board.set(
+      { x: 0, y: 0 },
+      { color: Color.Black, kind: PieceKind.Knight },
+    );
+  }
+
+  /**
+   * Move a piece at one position to another position.
+   * 
+   * This does nothing if there's no piece there
+   * 
+   * @param at the position to pick up a piece
+   * @param to the position to move a piece
+   */
+  move(at: Pos, to: Pos): boolean {
+    const piece = this.board.get(at);
+    if (piece === null) {
+      return false;
+    }
+    this.board.set(to, piece);
+    this.board.remove(at);
+    return true;
   }
 }
